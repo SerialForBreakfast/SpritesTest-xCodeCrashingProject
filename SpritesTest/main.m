8696a400042208e374c2098ace00c0d8231ca1fc
SerialForBreakfast <joe@ShowBlender-Portable.local>
2014-10-05 11:01:34 -0700

    Initial Commit

---
+++ SpritesTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SpritesTest
+//
+//  Created by Joe McCraw on 10/5/14.
+//  Copyright (c) 2014 ShowBlender. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
